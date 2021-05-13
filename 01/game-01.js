8ebed432e673d533cb3a6e61b202d9328a1c8776
Steven Nguyen <32990646+Tsurugi24@users.noreply.github.com>
2021-05-13 04:19:20 -0700

    Add files via upload

---
+++ 01/game-01.js
@@ -0,0 +1,15 @@
+const Game = function() {
+
+    this.color  = "rgb(0,0,0)";
+    this.colors = [0, 0, 0];
+    this.shifts = [1, 1, 1];
+
+    this.update = funciton() {
+
+        for (let index = 0; index < 3; index++)
+
+            let color = this.colors[index];
+            
+    }
+
+}
